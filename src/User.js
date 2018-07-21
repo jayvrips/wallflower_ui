8e52cb4e0a29a86bae046a69db2524e9bdafe3e6
cweisman <cypriana.weisman@mantech.com>
2018-07-21 11:29:45 -0700

    post_user failing on OPTIONS rest call

--- src/User.js
+++ src/User.js
@@ -1,7 +1,7 @@
 import $ from "jquery";
 import React from "react";
 import {connect} from 'react-redux';
-import {ajaxPut} from './common.js';
+import {ajaxPost, ajaxPut} from './common.js';
 
 //const User = ({users, get_users}) => {
 //    return (<div>{users[0].fullname}</div>);
@@ -9,29 +9,72 @@ import {ajaxPut} from './common.js';
 
 
 class User extends React.Component {
+    constructor(props) {
+       super(props);
+
+       this.user = {
+           name: React.createRef(),
+           fullname: React.createRef(),
+           password: React.createRef()
+       }
+    }
+
+/*
+    onFieldChange(field, value) {
+       let new_state = {};
+       new_state[field] = value;
+       this.setState(new_state);
+    }
+*/
+
     componentDidMount() {
-        this.props.get_users(this.props.users)
+        this.props.get_users(this.props.users);
+    }
+
+    renderForm() {
+        let self = this;
+
+	return(<div className="col_container">
+            <div>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" ref={this.user.name}/>
+            </div>
+
+            <div>
+            <label htmlFor="fullname">Full Name: </label>     
+            <input type="text" id="fullname" ref={this.user.fullname}/>
+            </div>
+
+            <div>
+            <label htmlFor="password">Password: </label>     
+            <input type="text" id="password" ref={this.user.password}/>
+            </div>
+
+            <div>
+                <input type="submit" value="submit" onClick={() => self.props.add_user(self.user)}/>
+            </div>
+        </div>);
     }
 
     render() {
-        if (Object.keys(this.props.users).length === 0)
-          return (<div>no users</div>)
-
         let users_list = [];
-        for (let user in this.props.users){
-            users_list.push(
-              <div>
-                <div>{this.props.users[user].fullname}</div>
-              </div>
-            )
+        if (Object.keys(this.props.users).length === 0)
+          users_list = (<div>no users</div>)
+
+        else{
+            for (let user in this.props.users){
+                users_list.push(
+                  <div>
+                    <div>{this.props.users[user].fullname}</div>
+                  </div>
+                )
+            }
         }
 
-        return <div>
+        return (<div>
             {users_list}
-            <div>
-                <input type="submit" value="submit" onClick={event => this.props.update_user(1)}/>
-            </div>
-          </div>;
+            {this.renderForm()}
+          </div>);
     }
 }
 
@@ -44,7 +87,7 @@ const mapDispatchToProps = dispatch => {
         get_users: (users) => {
             if (Object.keys(users).length !== 0)
                 return;
-            $.ajax("http://192.168.136.3:8000/users",
+            $.ajax("/users",
                 {
                     success: function(data) {
                         dispatch({type: 'UPDATE_USERS', payload: data});
@@ -56,11 +99,30 @@ const mapDispatchToProps = dispatch => {
                 }
             );
         },
+        add_user: (user) => {
+            let request_data = {
+                name: user.name.current.value,
+                fullname: user.fullname.current.value,
+                password: user.password.current.value
+            }
+            ajaxPost("/user",
+                    request_data,
+                    //rest call returns updated/new user
+                    //this anonymous function takes that data, and calls 
+                    //dispatch function to put in redux store
+                    function(response_data) {
+                        dispatch({type: 'UPDATE_USER', id: response_data['id'], user_data: response_data});
+                    },
+                    function(xhr, textStatus, errorThrown) {
+                        console.log("ERROR!!!!");
+                    }
+            );
+        },
         update_user: (user_id) => {
             let request_data = {name: "Ed",
                         fullname: "Ed Jones",
                         password: "password"};
-            ajaxPut("http://192.168.136.3:8000/users/"+ user_id,
+            ajaxPut("/users/"+ user_id,
                     request_data,
                     //rest call returns updated/new user
                     //this anonymous function takes that data, and calls 
--- src/common.js
+++ src/common.js
@@ -1,18 +1,26 @@
 import $ from "jquery";
 
-export function ajaxPut(url, data, success, error){
-   
-	 $.ajax(url,
-                {
-                    method: "PUT",
-                    success: success,
-                    error: error,
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    data: JSON.stringify(data),
-                    crossDomain: true
-                }
-         ) 
+function ajaxCall(method, path, data, success, error) {
+    let url = "http://192.168.43.129:8000" + path
+    $.ajax(url,
+        {
+            method: method,
+            success: success,
+            error: error,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(data),
+            crossDomain: true
+        }
+    );
+}
+
+export function ajaxPost(path, data, success, error) {
+    ajaxCall("POST", path, data, success, error);
+}
+
+export function ajaxPut(path, data, success, error) {
+    ajaxCall("PUT", path, data, success, error);
 }        
 
 
